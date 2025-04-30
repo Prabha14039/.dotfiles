eb90f33de1099542ce05bd4b06c63ecf01300cf1
Prabh14039 <abhinavg760@gmail.com>
2025-04-30 14:45:52 +0530

    zsh added

--- home.nix
+++ home.nix
@@ -1,15 +1,67 @@
-{ pkgs, ... }: {
-  home.username = "prabha14039";
-  home.homeDirectory = "/home/prabha14039";
-  home.stateVersion = "24.05";
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/home-manager):
+    # outputs.homeManagerModules.example
 
-  programs.neovim = {
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
   };
 
+  # TODO: Set your username
+  home = {
+    username = "prabha14039";
+    homeDirectory = "/home/prabha14039";
+    stateVersion =  "24.05";
+  };
+
+  # Add stuff for your user as you see fit:
+  programs.neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+  };
+
+  home.packages = with pkgs; [
+          python3
+          pkgs.go
+          pkgs.docker_28
+          pkgs.gh
+          micromamba
+  ];
+
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  #create symlinks
   home.file.".config/nvim".source = ./.config/nvim;
+  home.file.".zshrc".source = ./.config/zsh/.zshrc;
 }
 
